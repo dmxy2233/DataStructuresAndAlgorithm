8344756b2c85d235aa3f4e6366ec394ca7183fe9
jingluomin jingluomin <1176503736@qq.com>
2019-12-04 20:00:46 +0800

    百元买百鸡

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x;
+            int y;
+            int z;
+            for (x=1;x<=20;x++)
+            {
+                for (y = 1; y<= 33; y++)
+                {
+                    for (z=3;z<=99;z+=3)
+                    {
+                        if(5 * x + 3 * y + z / 3 == 100 &&x + y + z == 100)
+                        {
+
+                            Console.WriteLine("公鸡买"+x+"母鸡买"+y+"小鸡买"+z);
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+           
         }
     }
 }
