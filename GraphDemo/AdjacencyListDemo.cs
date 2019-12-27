0f50fc91cd59e2c59840c8659bdf97c2d2dcb513
LIzhengyv <768221774@qq.com>
2019-12-27 12:09:02 +0800

    一笔画作业（实现输入）

---
+++ GraphDemo/AdjacencyListDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS019.GraphDemo
+{
+    /// <summary>
+    /// 图的构建
+    /// </summary>
+    static class AdjacencyListDemo
+    {
+        public static AdjacencyList<string> GraphWithAdjacencyList(string [] dian,string [] dian1,string[] dian2)
+        {
+            var demoList = new AdjacencyList<string>();
+
+            for(int i=0;i< dian.Length;i++)
+                {
+                    demoList.AddVertex(dian[i]);
+                }
+           // demoList.AddVertex("A");
+           // demoList.AddVertex("B");
+          ///  demoList.AddVertex("C");
+          ///  demoList.AddVertex("D");
+            for(int y=0;y<dian.Length;y++)
+                {
+                 demoList.AddEdge(dian1[y], dian2[y]);
+                }
+            /*demoList.AddEdge("A", "B");
+            demoList.AddEdge("A", "C");
+            demoList.AddEdge("A", "D");
+            demoList.AddEdge("B", "D");
+            */
+            return demoList;
+        }
+
+    }
+}
---
+++ GraphDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDS019.GraphDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入顶点的数量");
+            int i = Convert.ToInt32( Console.ReadLine());
+            string[] shuru = new string[i];//接受用户输入的点名称
+            string[] Vertex = new string[100];
+            string[] Node = new string[100];
+            int count=0;
+             Console.WriteLine("请输入顶点的名称");
+            for (int x=0; x <i; x++)
+            {
+                shuru[x] = Console.ReadLine();
+            }
+            Console.WriteLine("请输入顶点,和普通点。输入完成");
+            while(true)
+                {
+                if(Console.ReadLine()=="输入完成")
+                    {
+                        break;
+                    }
+                    Vertex[count]=Console.ReadLine();
+                    Console.Write("-");
+                    Node[count]=Console.ReadLine();
+                    count++;
+                }
+            
+            var demoList01= AdjacencyListDemo.GraphWithAdjacencyList(shuru,Vertex,Node);
+            Console.WriteLine(demoList01);
+            Console.ReadKey();
+
+            //demoList01.InitVisited();
+            //demoList01.DepthFirstTraverse(demoList01.GetVertex("A"));
+            //Console.ReadKey();
+
+            demoList01.InitVisited();
+            demoList01.WidthFirstTraverse(demoList01.GetVertex("A"));
+            Console.ReadKey();
+
+        }
+    }
+}
